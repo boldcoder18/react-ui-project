fe331401a79353379ec1c051c60b85ee96031df3
boldcoder18 <boldurname@gmail.com>
2026-09-04 13:17:09 +0530

    Add home button

--- Gallery-project/src/pages/Nonext.jsx
+++ Gallery-project/src/pages/Nonext.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Nonext = () => {
+    const navigate = useNavigate()
     return (
-        <div className='h-full bg-amber-600 flex flex-wrap'>
-            <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://media.tenor.com/X5czfC8xXMEAAAAe/bas-kar.png" alt="ravi" />
-            <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxKkgFnH2ZRCJDLCl7h6dg_kRnum2O0NMQAMA85XY6HtJrUS-U-LjvrEmy&s=10" alt="ravi" />
-            <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpnYByydtExy9OaVZFhnjcL1OeVqttJIHIxhrwMvUvfHsl5IwbC8GoGCs&s=10" alt="ravi" />
-            <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReunEYwpCYDQobj_TFfXnDDGliuH8KGBaAUIB1ayD1ih9oLFV-KFf73mH-&s=10" alt="ravi" />
-            <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFFpXc51RHPEj8y2_E5lCRIOOD9Mod-rObj3g5LTrq-Q&s=10" alt="ravi" />
-            <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://i.ytimg.com/vi/ZzRDhAIscSs/hqdefault.jpg" alt="ravi" />
-        </div>
+        <div className='bg-amber-600 w-full'>
+            <button
+                onClick={() => {
+                    navigate('/')
+                }}
+                className='bg-amber-500 py-2 px-5 m-4 rounded cursor-pointer active:scale-95  '>
+                home
+            </button>
+            <div className='h-full bg-amber-600 flex flex-wrap'>
+
+                <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://media.tenor.com/X5czfC8xXMEAAAAe/bas-kar.png" alt="ravi" />
+                <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxKkgFnH2ZRCJDLCl7h6dg_kRnum2O0NMQAMA85XY6HtJrUS-U-LjvrEmy&s=10" alt="ravi" />
+                <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpnYByydtExy9OaVZFhnjcL1OeVqttJIHIxhrwMvUvfHsl5IwbC8GoGCs&s=10" alt="ravi" />
+                <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReunEYwpCYDQobj_TFfXnDDGliuH8KGBaAUIB1ayD1ih9oLFV-KFf73mH-&s=10" alt="ravi" />
+                <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFFpXc51RHPEj8y2_E5lCRIOOD9Mod-rObj3g5LTrq-Q&s=10" alt="ravi" />
+                <img className='h-100 w-100 object-cover rounded-2xl ml-2' src="https://i.ytimg.com/vi/ZzRDhAIscSs/hqdefault.jpg" alt="ravi" />
+            </div> </div>
     )
 }
 
